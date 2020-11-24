c37b286cf5a6e611dd03348ac8ec1a2926891425
IlhamAbiu <kh.ilgam1552@gmail.com>
2020-11-24 16:10:02 +0300

    hometask_5 change

--- app/src/main/java/ru/skillbranch/devintensive/ui/adapters/ChatAdapter.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/adapters/ChatAdapter.kt
@@ -16,7 +16,7 @@ import ru.skillbranch.devintensive.models.data.ChatItem
 import ru.skillbranch.devintensive.models.data.ChatType
 
 class ChatAdapter(val listener: (ChatItem) -> Unit) :
-    RecyclerView.Adapter<ChatAdapter.ChatItemViewHolder>() {
+        RecyclerView.Adapter<ChatAdapter.ChatItemViewHolder>() {
     var items: List<ChatItem> = listOf()
 
 
@@ -41,10 +41,10 @@ class ChatAdapter(val listener: (ChatItem) -> Unit) :
     fun updateData(data: List<ChatItem>) {
         val diffCallback = object : DiffUtil.Callback() {
             override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                items[oldItemPosition].id == data[newItemPosition].id
+                    items[oldItemPosition].id == data[newItemPosition].id
 
             override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                items[oldItemPosition].hashCode() == data[newItemPosition].hashCode()
+                    items[oldItemPosition].hashCode() == data[newItemPosition].hashCode()
 
             override fun getOldListSize(): Int = items.size
 
@@ -58,20 +58,20 @@ class ChatAdapter(val listener: (ChatItem) -> Unit) :
     }
 
     abstract inner class ChatItemViewHolder(override val containerView: View) :
-        RecyclerView.ViewHolder(containerView), LayoutContainer {
+            RecyclerView.ViewHolder(containerView), LayoutContainer {
         abstract fun bind(item: ChatItem, listener: (ChatItem) -> Unit)
     }
 
     abstract inner class TouchableChatItemViewHolder(override val containerView: View) :
-        ChatItemViewHolder(containerView), ItemTouchViewHolder {
+            ChatItemViewHolder(containerView), ItemTouchViewHolder {
 
 
         override fun onItemSelected() {
             val colorBackground = TypedValue()
             itemView.context.theme.resolveAttribute(
-                R.attr.colorItemSelectedBackground,
-                colorBackground,
-                true
+                    R.attr.colorItemSelectedBackground,
+                    colorBackground,
+                    true
             )
             itemView.setBackgroundColor(colorBackground.data)
         }
@@ -79,36 +79,36 @@ class ChatAdapter(val listener: (ChatItem) -> Unit) :
         override fun onItemCleared() {
             val colorBackground = TypedValue()
             itemView.context.theme.resolveAttribute(
-                R.attr.colorItemBackground,
-                colorBackground,
-                true
+                    R.attr.colorItemBackground,
+                    colorBackground,
+                    true
             )
             itemView.setBackgroundColor(colorBackground.data)
         }
     }
 
     inner class SingleViewHolder(override val containerView: View) :
-        TouchableChatItemViewHolder(containerView) {
+            TouchableChatItemViewHolder(containerView) {
         override fun bind(item: ChatItem, listener: (ChatItem) -> Unit) {
             if (item.avatar == null) {
                 Glide.with(itemView)
-                    .clear(iv_avatar_single)
+                        .clear(iv_avatar_single)
                 iv_avatar_single.setImageDrawable(
-                    containerView.resources.getDrawable(
-                        R.color.color_accent,
-                        containerView.context.theme
-                    )
+                        containerView.resources.getDrawable(
+                                R.color.color_accent,
+                                containerView.context.theme
+                        )
                 )
                 iv_avatar_single.setText(item.initials)
             } else {
                 iv_avatar_single.setText(null)
                 Glide.with(itemView)
-                    .load(item.avatar)
-                    .into(iv_avatar_single)
+                        .load(item.avatar)
+                        .into(iv_avatar_single)
 
             }
 
-//            sv_indicator.visibility = if (item.isOnline) View.VISIBLE else View.GONE
+            sv_indicator.visibility = if (item.isOnline) View.VISIBLE else View.GONE
 
             with(tv_date_single) {
                 visibility = if (item.lastMessageDate != null) View.VISIBLE else View.GONE
@@ -129,10 +129,10 @@ class ChatAdapter(val listener: (ChatItem) -> Unit) :
     }
 
     inner class GroupViewHolder(override val containerView: View) :
-        TouchableChatItemViewHolder(containerView) {
+            TouchableChatItemViewHolder(containerView) {
         override fun bind(item: ChatItem, listener: (ChatItem) -> Unit) {
             iv_avatar_group.setText(item.title[0].toString())
-            //sv_indicator.visibility = if (item.isOnline) View.VISIBLE else View.GONE
+            sv_indicator.visibility = if (item.isOnline) View.VISIBLE else View.GONE
 
             with(tv_date_group) {
                 visibility = if (item.lastMessageDate != null) View.VISIBLE else View.GONE
@@ -158,7 +158,7 @@ class ChatAdapter(val listener: (ChatItem) -> Unit) :
     }
 
     inner class ArhiveViewHolder(override val containerView: View) :
-        ChatItemViewHolder(containerView) {
+            ChatItemViewHolder(containerView) {
         override fun bind(item: ChatItem, listener: (ChatItem) -> Unit) {
             with(tv_date_archive) {
                 visibility = if (item.lastMessageDate != null) View.VISIBLE else View.GONE
